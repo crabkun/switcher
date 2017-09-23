bbf745a716160a19273655a277538f4b283b9edc
crabkun <529493022@qq.com>
2017-09-23 21:44:02 +0800

    1.0

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io/ioutil"
+	"encoding/json"
+	"net"
+	"log"
+	"./core"
+	"./bridge"
+)
+
+func main(){
+	var err error
+	log.Println("Switcher V1.0 by Crabkun")
+	buf,err:=ioutil.ReadFile("config.json")
+	if err!=nil{
+		panic("配置文件config.json载入失败:"+err.Error())
+	}
+	if err=json.Unmarshal(buf,&core.Config);err!=nil{
+		panic("配置文件config.json解析失败:"+err.Error())
+	}
+	core.InitRegExpMap()
+	l,err:=net.Listen("tcp",core.Config.ListenAddr)
+	if err!=nil{
+		panic("监听失败:"+err.Error())
+	}
+	log.Println("万能端口成功监听于",l.Addr())
+	for{
+		client,err:=l.Accept()
+		if err!=nil{
+			panic("客户端接受失败:"+err.Error())
+		}
+		go bridge.NewClientComming(client)
+	}
+}
